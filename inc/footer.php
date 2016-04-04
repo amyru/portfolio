928a594400a5a6f6a3445deb2e83b343c3a9e566
Amy Rudolph <amy.rudolph@live.co.uk>
2016-04-03 22:05:31 -0400

    cleaning up code and images

---
+++ inc/footer.php
@@ -0,0 +1,53 @@
+    <section id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading"><a href="mailto:amy.rudolph@live.co.uk">Let's Get In Touch!</a></h2>
+                    <hr class="light">
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="js/jquery.easing.min.js"></script>
+    <script src="js/jquery.fittext.js"></script>
+    <script src="js/wow.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/creative.js"></script>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <script type="text/javascript" src="js/jquery.swatchbook.js"></script>
+    <script type="text/javascript">
+        // $(function() {
+        
+        //     $( '#sb-container' ).swatchbook();
+        
+        // });
+        $(function() {
+            
+                $( '#sb-container' ).swatchbook( {
+                    // number of degrees that is between each item
+                    angleInc : 15,
+                    neighbor : 15,
+                    // if it should be closed by default
+                    initclosed : true,
+                    // index of the element that when clicked, triggers the open/close function
+                    // by default there is no such element
+                    closeIdx : 11
+                } );
+            
+            });
+    </script>
+
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,187 @@
+<?php   include("inc/header.php"); ?>
+
+    <header>
+        <div class="header-content">
+            <div class="header-content-inner">
+                <div class=".col-md-6 .col-md-offset-3">
+                <h1>Amy Rudolph</h1><br>
+
+                <p class="first-p">“All I have learned, I learned from books.”<br>― Abraham Lincoln</p>
+                <p class="first-p">Or in my case Google... I like this simple quote as it stands for continuous self learning.<br>
+                I am a web developer living in Toronto. I am passionate about writing code and the good that can be done with it.<br>
+                I have gained work experience in Frankfurt Germany, London, Manchester UK and Dubai UAE.</p>
+                </div>
+                
+                <a href="#portfolio" class="btn btn-primary btn-xl page-scroll header-btn">Go to Portfolio</a>
+            </div>
+        </div>
+    </header>
+
+    <section class="finasafe" id="portfolio">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-10 col-lg-offset-1 text-center">
+                    <h1 class="finasafe-header">Decentralized email encryption using the Blockchain</h1>
+                    <a href="finasafe.html" class="header-btn btn btn-default btn-xl skills page-scroll">View Project Notes</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="tpscrt">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <a href="#skills" class="tpscrt-btn btn btn-default btn-xl skills page-scroll">View Project Notes</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="ps">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <img class="ps-image" src='img/pslogo.png'><br>
+                    <!-- <h1>PrivacySnapper</h1> -->
+                    <img class="chrome"src="img/chrome.png"><img class="firefox" src="img/firefox.png"><br> <h3>Extension and Dashboard</h3>
+                    <a href="#skills" class="ps-btn btn btn-default btn-xl skills page-scroll">View Project Notes</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="pshell">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <img class="pshell-logo" src='img/privacyshell.png'><br>
+                    <img class="pshell-image" src='img/newspaper5.png'>
+                    <!-- <h1>PrivacySnapper</h1> -->
+                    <h3>Online Privacy News Site</h3>
+                    <a href="#skills" class="ps-btn btn btn-default btn-xl skills page-scroll">View Project Notes</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="D3js">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h3 class="d3-title">D3.js<br> Interactive Map</h3>
+                    <a href="#skills" class="ps-btn btn btn-default btn-xl skills page-scroll">View Project Notes</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="travelphotos">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <img class="travel-img" src='img/travelphotos.png'>
+                    <h2 class="travel-header">Photos from my travels with Emirates</h2>
+                    <a href="#skills" class="ps-btn btn btn-default btn-xl skills page-scroll">View Project Notes</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="main" id="skills">
+            <div class="row">
+                <h2 class="section-heading">SKILLS</h2>
+                <hr>
+            </div>
+            
+        <div id="sb-container" class="sb-container">
+        
+            <div>
+                <span class="sb-icon icon-cog turn"></span>
+                <h5><span>HTML5</span></h5>
+            </div>
+            <div>
+                <span class="sb-icon icon-flight"></span>
+                <h5><span>CSS3</span></h5>
+            </div>  
+            <div>
+                <span class="sb-icon icon-eye"></span>
+                <h5><span>JS</span></h5>
+            </div>  
+            <div>
+                <span class="sb-icon icon-install"></span>
+                <h5><span>jQuery</span></h5>
+            </div>  
+            <div>
+                <span class="sb-icon icon-bag"></span>
+                <h5><span>D3js</span></h5>
+            </div>  
+            <div>
+                <span class="sb-icon icon-globe"></span>
+                <h5><span>Ajax</span></h5>
+            </div>  
+            <div>
+                <span class="sb-icon icon-picture"></span>
+                <h5><span>Ruby</span></h5>
+            </div>  
+            <div>
+                <span class="sb-icon icon-video"></span>
+                <h5><span>PHP</span></h5>
+            </div>  
+            <div>
+                <span class="sb-icon icon-download"></span>
+                <h5><span>Rails</span></h5>
+            </div>  
+            <div>
+                <span class="sb-icon icon-mobile"></span>
+                <h5><span>Laravel</span></h5>
+            </div>
+            <div>
+                <span class="sb-icon icon-camera"></span>
+                <h5><span>SQL</span></h5>                                             
+            </div>
+            <div>
+                <h5><span>Click Me</span></h5>                                         
+            </div>      
+        </div><!-- sb-container -->
+    </section>
+    <section>
+        <div class="container"> 
+            <div class="row">
+                <h2 class="section-heading">Further Skills</h2>
+                <hr>
+            </div>
+            <div class="row">
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <img class="skillIcon" src="img/code_icon.png" alt="...">
+                        <div class="caption">
+                           Postgress > Wordpress <br>
+                           Unix > ReST<br>
+                           Git
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <img class="skillIcon" src="img/designer_icon.png" alt="...">              
+                        <div class="caption">
+                            User Experience<br>
+                            UI Design<br><br>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <img class="skillIcon" src="img/writer_icon.png" alt="...">
+                        <div class="caption">
+                            Editing<br>
+                            Copy Writing<br>
+                            Growth Hacking and SEO<br>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+<?php include('inc/footer.php'); ?>
